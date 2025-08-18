f0337850af420fdab5f237ed5729797add3a6dcb
djinylh <d.jinylh@gmail.com>
2025-08-18 11:22:06 +0900

    프로필 사진 삭제 처리

--- src/components/ProfileImg.vue
+++ src/components/ProfileImg.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { ref, computed } from 'vue';
+import {useAuthenticationStore} from '@/stores/authentication.js';
 import defaultProfileImg from '@/assets/defaultProfileImg.png';
+
+const authenticationStore = useAuthenticationStore();
 
 const baseUrl = ref(import.meta.env.VITE_BASE_URL);
 
@@ -8,13 +11,17 @@ const props = defineProps({
     userId: Number,
     clsValue: String,
     size: Number,
-    pic: String  
+    pic: String
 });
 
-console.log('profileImg - props:', `${baseUrl.value}/pic/profile/${props.userId}/${props.pic}` )
+console.log('profileImg - props:', `${baseUrl.value}/pic/profile/${props.userId}/${props.pic}`)
 
 //props값을 변수에 재할당해서 사용하는 경우 반응성이 사라짐. 이때는 computed를 사용하거나 toRefs 사용해야 함. 아래는 computed로 해결
-const pic = computed( () => props.pic ? `${baseUrl.value}/pic/profile/${props.userId}/${props.pic}` : defaultProfileImg );
+const pic = computed(() => props.pic 
+        ? props.userId === authenticationStore.state.signedUser.userId
+        ? `${baseUrl.value}/pic/profile/${props.userId}/${authenticationStore.state.signedUser.pic}`
+        : `${baseUrl.value}/pic/profile/${props.userId}/${props.pic}`
+    : defaultProfileImg);
 
 const handleImgError = e => {
     e.target.src = defaultProfileImg;
@@ -30,14 +37,42 @@ const handleImgError = e => {
 </template>
 
 <style scoped>
-.w24 { width: 24px; }
-.w30 { width: 30px; }
-.w100 { width: 100px; }
-.w250 { width: 250px; }
-.w300 { width: 300px; }
+.w24 {
+    width: 24px;
+}
 
-.w800 { width: 800px; }
+.w30 {
+    width: 30px;
+}
 
-.circleimg { display: inline-block; overflow: hidden; border-radius: 50%; border: 1px solid #ecf0f1; vertical-align: top; }
-.circleimg > img { width: 100%; height: 100%; object-fit: cover; vertical-align: top; }
+.w100 {
+    width: 100px;
+}
+
+.w250 {
+    width: 250px;
+}
+
+.w300 {
+    width: 300px;
+}
+
+.w800 {
+    width: 800px;
+}
+
+.circleimg {
+    display: inline-block;
+    overflow: hidden;
+    border-radius: 50%;
+    border: 1px solid #ecf0f1;
+    vertical-align: top;
+}
+
+.circleimg>img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    vertical-align: top;
+}
 </style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -5,7 +5,7 @@ import FeedCard from '@/components/FeedCard.vue';
 import { ref, reactive, onMounted, onUnmounted } from 'vue';
 import { useRoute, useRouter, onBeforeRouteUpdate  } from 'vue-router';
 import { useAuthenticationStore } from '@/stores/authentication';
-import { getUserProfile, patchUserProfilePic } from '@/services/userService';
+import { getUserProfile, patchUserProfilePic, deleteUserProfilePic } from '@/services/userService';
 import { postUserFollow, deleteUserFollow } from '@/services/followService';
 import { getFeedList } from '@/services/feedService';
 import { bindEvent } from '@/utils/commonUtils';
@@ -118,8 +118,14 @@ const getFeedData = async () => {
     state.isLoading = false;
 }
 
-const removeUserPic = () => {
+const removeUserPic = async () => {
     console.log('프로파일 이미지 삭제');
+
+    const res = await deleteUserProfilePic();
+    if(res.status === 200) {
+        state.userProfile.pic = null;
+        authenticationStore.setSigndUserPic(null);
+    }   
 }
 
 const onClickProfileImg = () => {
